c4f35df702d83d31c0c3442624af39f78ff65869
vicheanath <nathvichea1@gmail.com>
2020-08-28 21:16:12 +0700

    our destination

--- js/index.js
+++ js/index.js
@@ -9,6 +9,48 @@ $('#nav').click(function () {
 
 $('.pro-item').mouseover(function () {
     $(this).find('.img').css({ 'transform': 'scale(1.1)' })
-}).mouseout(function() {
+}).mouseout(function () {
     $(this).find('.img').css({ 'transform': 'scale(1)' })
-  })
+})
+
+// back-to-top
+$(window).scroll(function () {
+    // Show button after 100px
+    var showAfter = 100;
+    if ($(this).scrollTop() > showAfter) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+
+//Click event to scroll to top
+$('#back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+});
+$('.img-box').click(function () {
+    $('#popup').css({ 'display': 'block' })
+    var img = $(this).find('img').attr("src");
+    //    console.dir(img[0].attributes[0].value)
+    //    console.dir(img)
+    $('body').find('#img-dailog').attr('src', img)
+})
+$('.delet').click(function () {
+    $('body').find('#popup').css({ 'display': 'none' })
+})
+$('#btn-donwload').click(function () {
+    var imageUrl = $('body').find('.img-dailog').attr("src")
+    $.ajax({ 
+        processData : false,
+        url: imageUrl, 
+        success: function(data) {
+            console.log(data)
+        },
+        error: function(r,x) {
+     
+        }
+     }); 
+})
+
+
